a5a590b2a75030ea2cc74b334dd79d760445ca45
sinansoezen <sinan.soezen@sblum.de>
2020-08-06 13:53:44 +0200

    added dependency injection

---
+++ src/ExceptionEnricher/DependencyInjection/ExceptionEnricherExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExceptionEnricher\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Config\FileLocator;
+
+class ExceptionEnricherExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(\dirname(__DIR__).'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
