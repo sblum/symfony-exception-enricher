b99045c787b3f75e612b990b32c566c3ee47bd93
sinansoezen <sinan.soezen@sblum.de>
2020-08-06 11:19:06 +0200

    initial commit

---
+++ src/ExceptionEnricher/Processor/ExceptionEnricherProcessor.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExceptionEnricher\Processor;
+
+use Monolog\Processor\ProcessorInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ExceptionEnricherProcessor implements ProcessorInterface
+{
+    /** @var string $postParams */
+    private $postParams = null;
+    /** @var RequestStack $requestStack */
+    private $requestStack;
+    /** @var SessionInterface $session */
+    private $session;
+    /** @var UserInterface $user */
+    private $user;
+
+    public function __construct(?RequestStack $requestStack, ?SessionInterface $session, ?UserInterface $user)
+    {
+        $this->requestStack = $requestStack;
+        $this->session = $session;
+        $this->user = $user;
+    }
+
+    public function __invoke(array $record)
+    {
+        if ($this->requestStack) {
+            if ($this->requestStack->getCurrentRequest()->getRequestUri()) {
+                $record['extra']['request_uri'] = \sprintf('%s %s', $this->requestStack->getCurrentRequest()->getMethod(), $this->requestStack->getCurrentRequest()->getRequestUri());
+            }
+
+            if ('POST' === $this->requestStack->getCurrentRequest()->getMethod()) {
+                $postParams = $this->requestStack->getCurrentRequest()->request->all();
+
+                if (false === empty($postParams)) {
+                    $this->postParams = \serialize($postParams);
+                    $record['extra']['request_post_data'] = $this->postParams;
+                }
+            }
+
+            if ($this->requestStack->getCurrentRequest()->headers) {
+                $record['extra']['request_user_agent'] = $this->requestStack->getCurrentRequest()->headers->get('User-Agent');
+            }
+
+            if ($this->requestStack->getMasterRequest()) {
+                $record['extra']['request_ip'] = $this->requestStack->getMasterRequest()->getClientIp();
+            }
+        }
+
+        if ($this->session) {
+            $record['extra']['session_id'] = $this->session->getId();
+        }
+
+        if ($this->user) {
+            $record['extra']['username'] = $this->user->getUsername();
+        }
+
+        return $record;
+    }
+}
---
+++ tests/ExceptionEnricher/Processor/ExceptionEnricherProcessorTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExceptionEnricher\Processor;
+
+use DateTimeImmutable;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ExceptionEnricherProcessorTest extends TestCase
+{
+    /**
+     * @covers \ExceptionEnricherProcessor::__invoke
+     */
+    public function testEmptyProcessor()
+    {
+        $exceptionEnricherProcessor = new ExceptionEnricherProcessor(null, null, null);
+        $record = $exceptionEnricherProcessor($this->createRecord());
+
+        $this->assertArrayNotHasKey('request_uri', $record['extra']);
+        $this->assertArrayNotHasKey('request_post_data', $record['extra']);
+        $this->assertArrayNotHasKey('request_user_agent', $record['extra']);
+        $this->assertArrayNotHasKey('request_ip', $record['extra']);
+        $this->assertArrayNotHasKey('session_id', $record['extra']);
+        $this->assertArrayNotHasKey('username', $record['extra']);
+        $this->assertTrue(empty($record['extra']));
+    }
+
+    /**
+     * @covers \ExceptionEnricherProcessor::__invoke
+     */
+    public function testFullProcessor()
+    {
+        $request = $this->prophesize(Request::class);
+        $request->headers = new HeaderBag(['User-Agent' => 'Mozilla/5.0']);
+        $request->getClientIp()->willReturn('127.0.0.1')->shouldBeCalled();
+        $request->getMethod()->willReturn('GET')->shouldBeCalled();
+        $request->getRequestUri()->willReturn('/testroute/')->shouldBeCalled();
+
+        $requestStack = $this->prophesize(RequestStack::class);
+        $requestStack->getCurrentRequest()->willReturn($request->reveal())->shouldBeCalled();
+        $requestStack->getMasterRequest()->willReturn($request->reveal())->shouldBeCalled();
+
+        $session = $this->prophesize(SessionInterface::class);
+        $session->getId()->willReturn('39d9f31fb12441428031e26d2f83ab6e')->shouldBeCalled();
+
+        $user = $this->prophesize(UserInterface::class);
+        $user->getUsername()->willReturn('testuser')->shouldBeCalled();
+
+        $exceptionEnricherProcessor = new ExceptionEnricherProcessor($requestStack->reveal(), $session->reveal(), $user->reveal());
+        $record = $exceptionEnricherProcessor($this->createRecord());
+
+        $this->assertArrayHasKey('request_uri', $record['extra']);
+        $this->assertArrayHasKey('request_user_agent', $record['extra']);
+        $this->assertArrayHasKey('request_ip', $record['extra']);
+        $this->assertArrayHasKey('session_id', $record['extra']);
+        $this->assertArrayHasKey('username', $record['extra']);
+        $this->assertArrayNotHasKey('request_post_data', $record['extra']);
+
+        $this->assertSame('GET /testroute/', $record['extra']['request_uri']);
+        $this->assertSame('Mozilla/5.0', $record['extra']['request_user_agent']);
+        $this->assertSame('127.0.0.1', $record['extra']['request_ip']);
+        $this->assertSame('testuser', $record['extra']['username']);
+        $this->assertSame('39d9f31fb12441428031e26d2f83ab6e', $record['extra']['session_id']);
+    }
+
+    private function createRecord($level = Logger::ERROR, $message = 'An Exception has been encountered.'): array
+    {
+        return [
+            'message' => $message,
+            'level' => $level,
+            'level_name' => Logger::getLevelName($level),
+            'channel' => 'test',
+            'datetime' => new DateTimeImmutable('now'),
+            'extra' => [],
+        ];
+    }
+}
